ce3126cd135a92f68b82402cd9c37dfdc28847f5
NguyenHaIT2 <54541978+NguyenHaIT2@users.noreply.github.com>
2021-10-04 15:28:05 +0700

    Update user_feature_views.py

--- django_apis/main/views/user_feature_views.py
+++ django_apis/main/views/user_feature_views.py
@@ -11,8 +11,9 @@ from rest_framework.views import APIView
 from rest_framework.generics import GenericAPIView, CreateAPIView
 from rest_framework.viewsets import GenericViewSet
 
-from tutorials.models import user_feature
-from tutorials.serializers import TutorialSerializer, RegistrationSerializer
+from ..models.models import user_feature
+from ..serializers.serializers import User_featureSerializer
+
 from rest_framework.decorators import api_view
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 import kwargs as kwargs
@@ -34,15 +35,15 @@ from core.record import check_user, record_log_in, record_signup, _login
 @api_view(['POST'])
 def registration_view(request):
     if request.method == 'POST':
-        tutorial_data = JSONParser().parse(request)
-        tutorial_serializer = TutorialSerializer(data=tutorial_data)
-        if tutorial_serializer.is_valid():
-            tutorial_serializer.save()
-            username = tutorial_serializer.data['username']
-            vector = tutorial_serializer.data['vector']
+        user_data = JSONParser().parse(request)
+        user_serializer = User_featureSerializer(data=user_data)
+        if user_serializer.is_valid():
+            user_serializer.save()
+            username = user_serializer.data['username']
+            vector = user_serializer.data['vector']
             record_signup(username, vector)
-            return JsonResponse(tutorial_serializer.data, status=status.HTTP_201_CREATED)
-        return JsonResponse(tutorial_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+            return JsonResponse(user_serializer.data, status=status.HTTP_201_CREATED)
+        return JsonResponse(user_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 # Login
@@ -50,17 +51,17 @@ def registration_view(request):
 def login(request):
     result = record_log_in()
     request.method = 'GET'
-    tutorials = user_feature.objects.filter(username=result)
-    tutorials_serializer = TutorialSerializer(tutorials, many=True)
-    return JsonResponse(tutorials_serializer.data, safe=False)
+    user = user_feature.objects.filter(username=result)
+    user_serializer = User_featureSerializer(user, many=True)
+    return JsonResponse(user_serializer.data, safe=False)
 
 @api_view(['GET'])
 def login2(request, file_name):
-    result = _login(file_name)
+    resule = _login(file_name)
     request.method = 'GET'
-    tutorials = user_feature.objects.filter(username = result)
-    tutorials_serializer = TutorialSerializer(tutorials, many=True)
-    return JsonResponse(tutorials_serializer.data, safe=False)
+    user = user_feature.objects.filter(username = result)
+    user_serializer = User_featureSerializer(user, many=True)
+    return JsonResponse(user_serializer.data, safe=False)
 
 @api_view(['GET', 'POST', 'DELETE'])
 def tutorial_list(request):
@@ -71,13 +72,13 @@ def tutorial_list(request):
         if username is not None:
             tutorials = tutorials.filter(title__icontains=username)
 
-        tutorials_serializer = TutorialSerializer(tutorials, many=True)
+        tutorials_serializer = User_featureSerializer(tutorials, many=True)
         return JsonResponse(tutorials_serializer.data, safe=False)
         # 'safe=False' for objects serialization
 
     elif request.method == 'POST':
         tutorial_data = JSONParser().parse(request)
-        tutorial_serializer = TutorialSerializer(data=tutorial_data)
+        tutorial_serializer = User_featureSerializer(data=tutorial_data)
         if tutorial_serializer.is_valid():
             tutorial_serializer.save()
             return JsonResponse(tutorial_serializer.data, status=status.HTTP_201_CREATED)
@@ -97,12 +98,12 @@ def tutorial_detail(request, pk):
         return JsonResponse({'message': 'The User does not exist'}, status=status.HTTP_404_NOT_FOUND)
 
     if request.method == 'GET':
-        tutorial_serializer = TutorialSerializer(tutorial)
+        tutorial_serializer = User_featureSerializer(tutorial)
         return JsonResponse(tutorial_serializer.data)
 
     elif request.method == 'PUT':
         tutorial_data = JSONParser().parse(request)
-        tutorial_serializer = TutorialSerializer(tutorial, data=tutorial_data)
+        tutorial_serializer = User_featureSerializer(tutorial, data=tutorial_data)
         if tutorial_serializer.is_valid():
             tutorial_serializer.save()
             return JsonResponse(tutorial_serializer.data)
